2ce8ee320e3d4bef71664f32484abaf2fdf04d8c
Park MinSeong <qkralstjd02@gmail.com>
2024-11-11 19:33:56 +0900

    테스트 데이터 추가

---
+++ app/src/main/java/kr/ilf/kshoong/data/SwimData.kt
@@ -0,0 +1,10 @@
+package kr.ilf.kshoong.data
+
+data class SwimData(
+    val date: String,
+    val freestyle: Int?,
+    val backstroke: Int?,
+    val breaststroke: Int?,
+    val butterfly: Int?,
+    val kickBoard: Int?
+)
