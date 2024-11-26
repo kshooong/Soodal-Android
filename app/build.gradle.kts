4ec2acc16ee7f4953d96a88a632195399d171734
Park MinSeong <qkralstjd02@gmail.com>
2024-11-26 13:59:26 +0900

    room 종속성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.room.runtime)
+    annotationProcessor(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
