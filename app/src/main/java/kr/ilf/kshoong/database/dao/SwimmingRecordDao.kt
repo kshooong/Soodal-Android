b6a1b2af6deed61c9c35459e2eaac21e8c0505c1
Park MinSeong <qkralstjd02@gmail.com>
2024-11-27 23:53:14 +0900

    room 데이터 베이스 추가

---
+++ app/src/main/java/kr/ilf/kshoong/database/dao/SwimmingRecordDao.kt
@@ -0,0 +1,86 @@
+package kr.ilf.kshoong.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Update
+import kr.ilf.kshoong.database.DatabaseConst
+import kr.ilf.kshoong.database.entity.DailyRecord
+import kr.ilf.kshoong.database.entity.DailyRecordWithDetailRecord
+import kr.ilf.kshoong.database.entity.DetailRecord
+import java.time.Instant
+
+@Dao
+interface SwimmingRecordDao {
+
+    /*
+    * DailyRecordWithDetailRecord
+    */
+    @Query("SELECT * FROM ${DatabaseConst.TB_DAILY_RECORD} ORDER BY date DESC")
+    fun getAllRecords(): List<DailyRecordWithDetailRecord>
+
+    @Transaction
+    fun insertDailyRecordWithDetailRecord(
+        dailyRecord: DailyRecord,
+        detailRecord: List<DetailRecord>
+    ) {
+        insertDailyRecord(dailyRecord)
+        insertDetailRecords(detailRecord)
+    }
+
+    /*
+    * DailyRecord
+    */
+    @Query("SELECT * FROM ${DatabaseConst.TB_DAILY_RECORD} WHERE date = :date")
+    fun getDailyRecordWithDetailRecord(date: Instant)
+
+    @Query("SELECT * FROM ${DatabaseConst.TB_DAILY_RECORD} WHERE date = :date")
+    fun getDailyRecord(date: Instant): DailyRecord?
+
+    @Insert
+    fun insertDailyRecord(dailyRecord: DailyRecord)
+
+    @Insert
+    fun insertDailyRecords(dailyRecord: List<DailyRecord>)
+
+    @Update
+    fun updateDailyRecord(dailyRecord: DailyRecord)
+
+    @Update
+    fun updateDailyRecords(dailyRecord: List<DailyRecord>)
+
+    @Delete
+    fun deleteDailyRecord(dailyRecord: DailyRecord)
+
+    @Delete
+    fun deleteDailyRecords(dailyRecord: List<DailyRecord>)
+
+    /*
+    * DetailRecord
+    */
+    @Query("SELECT * FROM ${DatabaseConst.TB_DETAIL_RECORD} WHERE id = :id")
+    fun getDetailRecord(id: String): DetailRecord
+
+    @Query("SELECT * FROM ${DatabaseConst.TB_DETAIL_RECORD} WHERE date = :date")
+    fun getDetailRecordsByDate(date: Instant): List<DetailRecord>
+
+    @Insert
+    fun insertDetailRecord(detailRecord: DetailRecord)
+
+    @Insert
+    fun insertDetailRecords(detailRecord: List<DetailRecord>)
+
+    @Update
+    fun updateDetailRecord(detailRecord: DetailRecord)
+
+    @Update
+    fun updateDetailRecords(detailRecord: List<DetailRecord>)
+
+    @Delete
+    fun deleteDetailRecord(detailRecord: DetailRecord)
+
+    @Delete
+    fun deleteDetailRecords(detailRecord: List<DetailRecord>)
+}
