a6b03ce50fe78329bd9076e79fd3df92a6535495
Park MinSeong <qkralstjd02@gmail.com>
2024-11-27 23:53:14 +0900

    room 데이터 베이스 추가

---
+++ app/src/main/java/kr/ilf/kshoong/database/DatabaseConst.kt
@@ -0,0 +1,7 @@
+package kr.ilf.kshoong.database
+
+object DatabaseConst {
+    const val DATABASE_VERSION = 1
+    const val TB_DAILY_RECORD = "tb_daily_record"
+    const val TB_DETAIL_RECORD = "tb_detail_record"
+}
---
+++ app/src/main/java/kr/ilf/kshoong/database/converter/InstantConverter.kt
@@ -0,0 +1,17 @@
+package kr.ilf.kshoong.database.converter
+
+import androidx.room.TypeConverter
+import java.time.Instant
+
+
+class InstantConverter {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Instant? {
+        return value?.let { Instant.ofEpochMilli(it) }
+    }
+
+    @TypeConverter
+    fun instantToTimestamp(instant: Instant?): Long? {
+        return instant?.toEpochMilli()
+    }
+}
