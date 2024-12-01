eb44513206361fbddf24c8d4aa14850fb17ab45c
Park MinSeong <qkralstjd02@gmail.com>
2024-12-02 00:23:50 +0900

    Navigation 적용 / 로딩 -> 싱크화면 띄우기까지 로직 구현

---
+++ app/src/main/java/kr/ilf/kshoong/ui/NavigationView.kt
@@ -0,0 +1,151 @@
+package kr.ilf.kshoong.ui
+
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import kotlinx.coroutines.delay
+import kr.ilf.kshoong.Destinations
+import kr.ilf.kshoong.HealthConnectManager
+import kr.ilf.kshoong.LoadingView
+import kr.ilf.kshoong.R
+import kr.ilf.kshoong.viewmodel.SwimmingViewModel
+import kr.ilf.kshoong.viewmodel.SwimmingViewModelFactory
+
+@Composable
+fun NavigationView(navController: NavHostController, healthConnectManager: HealthConnectManager) {
+    val context = LocalContext.current
+    val viewModel: SwimmingViewModel =
+        viewModel(factory = SwimmingViewModelFactory(healthConnectManager))
+
+    NavHost(navController = navController, startDestination = Destinations.DESTINATION_LOADING) {
+        composable(Destinations.DESTINATION_LOADING) {
+            LoadingView(
+                context = context,
+                healthConnectManager = healthConnectManager,
+                viewModel = viewModel,
+                onLoadingComplete = { navController.navigate(Destinations.DESTINATION_SYNC) })
+        }
+        composable(Destinations.DESTINATION_SYNC) {
+            SyncView()
+        }
+        composable(Destinations.DESTINATION_CALENDAR) {
+
+        }
+    }
+}
+
+@Composable
+fun LoadingView(
+    context: Context,
+    healthConnectManager: HealthConnectManager,
+    viewModel: SwimmingViewModel,
+    onLoadingComplete: () -> Unit
+) {
+    fun setChangeToken() {
+        val sharedPreferences = context.getSharedPreferences("changeToken", MODE_PRIVATE)
+        viewModel.setChangeToken(sharedPreferences.getString("changeToken", null))
+        onLoadingComplete()
+    }
+
+    val availability by healthConnectManager.availability
+    if (availability && viewModel.hasAllPermissions.value.not()) {
+        val permissions = viewModel.healthPermissions
+        val permissionsLauncher =
+            rememberLauncherForActivityResult(contract = viewModel.permissionsContract) {
+                // Handle permission result/
+                if (viewModel.checkPermissions()) {
+                    setChangeToken()
+                } else {
+                    // 종료
+                }
+            }
+
+        LaunchedEffect(Unit) {
+            delay(1000)
+            permissionsLauncher.launch(permissions)
+        }
+    } else if (availability && viewModel.hasAllPermissions.value) {
+        LaunchedEffect(Unit) {
+            delay(1000)
+            setChangeToken()
+        }
+    } else {
+        // 종료
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White, shape = ShapeDefaults.ExtraLarge),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .wrapContentSize()
+                .align(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "KSHOONG!", style = MaterialTheme.typography.titleLarge)
+            Image(
+                painter = painterResource(id = R.drawable.logo_loading),
+                contentDescription = "logo",
+                modifier = Modifier.size(300.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun SyncView(
+//    context: Context,
+//    healthConnectManager: HealthConnectManager,
+//    viewModel: SwimmingViewModel,
+//    onLoadingComplete: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White, shape = ShapeDefaults.ExtraLarge),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .wrapContentSize()
+                .align(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Synchronizing", style = MaterialTheme.typography.titleLarge)
+            Image(
+                painter = painterResource(id = R.drawable.logo_loading),
+                contentDescription = "logo",
+                modifier = Modifier.size(300.dp)
+            )
+        }
+    }
+}
