2aa736affbd92eb34a09fe4eb42157cccb746883
Park MinSeong <qkralstjd02@gmail.com>
2024-12-02 00:23:50 +0900

    Navigation 적용 / 로딩 -> 싱크화면 띄우기까지 로직 구현

---
+++ app/src/main/java/kr/ilf/kshoong/Destinations.kt
@@ -0,0 +1,9 @@
+package kr.ilf.kshoong
+
+interface Destinations {
+    companion object{
+        const val DESTINATION_LOADING = "loading"
+        const val DESTINATION_SYNC = "sync"
+        const val DESTINATION_CALENDAR = "calendar"
+    }
+}
--- app/src/main/java/kr/ilf/kshoong/viewmodel/SwimmingViewModel.kt
+++ app/src/main/java/kr/ilf/kshoong/viewmodel/SwimmingViewModel.kt
@@ -1,13 +1,63 @@
 package kr.ilf.kshoong.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.health.connect.client.permission.HealthPermission
+import androidx.health.connect.client.records.DistanceRecord
+import androidx.health.connect.client.records.ExerciseSessionRecord
+import androidx.health.connect.client.records.HeartRateRecord
+import androidx.health.connect.client.records.SpeedRecord
+import androidx.health.connect.client.records.TotalCaloriesBurnedRecord
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import kr.ilf.kshoong.HealthConnectManager
+import kr.ilf.kshoong.database.entity.DailyRecord
 
 class SwimmingViewModel(
     private val healthConnectManager: HealthConnectManager
 ) : ViewModel() {
 
+    val healthPermissions =
+        setOf(
+            HealthPermission.getReadPermission(ExerciseSessionRecord::class),
+            HealthPermission.getWritePermission(ExerciseSessionRecord::class),
+            HealthPermission.getReadPermission(SpeedRecord::class),
+            HealthPermission.getWritePermission(SpeedRecord::class),
+            HealthPermission.getReadPermission(DistanceRecord::class),
+            HealthPermission.getWritePermission(DistanceRecord::class),
+            HealthPermission.getReadPermission(HeartRateRecord::class),
+            HealthPermission.getWritePermission(HeartRateRecord::class),
+            HealthPermission.getReadPermission(TotalCaloriesBurnedRecord::class),
+            HealthPermission.getWritePermission(TotalCaloriesBurnedRecord::class),
+        )
+    val hasAllPermissions = mutableStateOf(false)
+    val permissionsContract = healthConnectManager.requestPermissionActivityContract()
+
+    val changeToken = mutableStateOf<String?>(null)
+
+    private val _swimmingData = MutableStateFlow<MutableList<DailyRecord>>(mutableListOf())
+    val swimmingData
+        get() = _swimmingData
+
+    init {
+        viewModelScope.launch {
+            hasAllPermissions.value = healthConnectManager.checkPermissions(healthPermissions)
+        }
+    }
+
+    fun checkPermissions(): Boolean {
+        viewModelScope.launch {
+            hasAllPermissions.value = healthConnectManager.checkPermissions(healthPermissions)
+        }
+
+        return hasAllPermissions.value
+    }
+
+    fun setChangeToken(token: String?) {
+        changeToken.value = token
+    }
 }
 
 class SwimmingViewModelFactory(
