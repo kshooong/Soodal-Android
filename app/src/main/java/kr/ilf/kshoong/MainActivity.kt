dd941cdd48321aff00a5e02bb60aff0ef6fd49fe
Park MinSeong <qkralstjd02@gmail.com>
2024-12-09 18:02:04 +0900

    temp

--- app/src/main/java/kr/ilf/kshoong/MainActivity.kt
+++ app/src/main/java/kr/ilf/kshoong/MainActivity.kt
@@ -5,8 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
@@ -29,7 +38,7 @@ class MainActivity : ComponentActivity() {
         })
 
         splashScreen.setOnExitAnimationListener { splashScreenView ->
-          splashScreenView.remove()
+            splashScreenView.remove()
         }
 
         enableEdgeToEdge()
@@ -38,8 +47,46 @@ class MainActivity : ComponentActivity() {
                 val viewModel: SwimmingViewModel =
                     viewModel(factory = SwimmingViewModelFactory(application, healthConnectManager))
                 val navController = rememberNavController()
-                NavigationView(navController, healthConnectManager, viewModel)
+                Box(modifier = Modifier.fillMaxSize()) {
+                    NavigationView(navController, healthConnectManager, viewModel)
+                    NavigationBarView(
+                        modifier = Modifier
+                            .height(80.dp)
+                            .fillMaxWidth()
+                            .align(Alignment.BottomCenter),
+                        {
+                            if (navController.currentDestination?.route != Destination.Calendar.route)
+                                navController.navigate(Destination.Calendar.route) {
+                                    launchSingleTop = true
+                                    popUpTo(Destination.Calendar.route) {
+                                        inclusive = true
+                                    }
+                                }
+                        },
+                        {
+                            if (navController.currentDestination?.route != Destination.Detail.route)
+                                navController.navigate(Destination.Detail.route) {
+                                    launchSingleTop = true
+                                    popUpTo(Destination.Detail.route) {
+                                        inclusive = true
+                                    }
+                                }
+                        },
+                    )
+                }
             }
+        }
+    }
+}
+
+@Composable
+fun NavigationBarView(modifier: Modifier, onCalenderClick: () -> Unit, onDetailClick: () -> Unit) {
+    Row(modifier = modifier) {
+        Button(onClick = { onCalenderClick() }) {
+
+        }
+        Button(onClick = { onDetailClick() }) {
+
         }
     }
 }
