156768c8b228774b26781de5ccaac9a23436978c
Park MinSeong <qkralstjd02@gmail.com>
2025-01-16 17:41:06 +0900

    데이타베이스 버전 1로 변경

--- app/src/main/java/kr/ilf/kshoong/database/database/SwimmingRecordDatabase.kt
+++ app/src/main/java/kr/ilf/kshoong/database/database/SwimmingRecordDatabase.kt
@@ -34,50 +34,11 @@ abstract class SwimmingRecordDatabase : RoomDatabase() {
                     instance = Room.databaseBuilder(
                         context.applicationContext, SwimmingRecordDatabase::class.java,
                         "DailyRecordDatabase"
-                    ).addMigrations(migration_1_2, migration_2_3).build()
+                    ).build()
                 }
             }
 
             return instance
         }
-    }
-}
-
-
-val migration_1_2 = object : Migration(1, 2) {
-    override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL(
-            """
-            CREATE TABLE ${DatabaseConst.TB_HEARTRATE_SAMPLE} (
-                time INTEGER NOT NULL PRIMARY KEY, 
-                detailRecordId TEXT NOT NULL, 
-                beatsPerMinute INTEGER NOT NULL,
-                FOREIGN KEY(detailRecordId) REFERENCES ${DatabaseConst.TB_DETAIL_RECORD}(id) ON DELETE CASCADE ON UPDATE CASCADE
-            )
-        """.trimIndent()
-        )
-    }
-}
-
-val migration_2_3 = object : Migration(2, 3) {
-    override fun migrate(database: SupportSQLiteDatabase) {
-
-        database.execSQL("ALTER TABLE ${DatabaseConst.TB_DAILY_RECORD} ADD COLUMN 'crawl' INTEGER NOT NULL DEFAULT 0;")
-        database.execSQL("ALTER TABLE ${DatabaseConst.TB_DAILY_RECORD} ADD COLUMN 'backStroke' INTEGER NOT NULL DEFAULT 0;")
-        database.execSQL("ALTER TABLE ${DatabaseConst.TB_DAILY_RECORD} ADD COLUMN 'breastStroke' INTEGER NOT NULL DEFAULT 0;")
-        database.execSQL("ALTER TABLE ${DatabaseConst.TB_DAILY_RECORD} ADD COLUMN 'butterfly' INTEGER NOT NULL DEFAULT 0;")
-        database.execSQL("ALTER TABLE ${DatabaseConst.TB_DAILY_RECORD} ADD COLUMN 'kickBoard' INTEGER NOT NULL DEFAULT 0;")
-        database.execSQL("ALTER TABLE ${DatabaseConst.TB_DAILY_RECORD} ADD COLUMN 'mixed' INTEGER NOT NULL DEFAULT 0;")
-        database.execSQL("UPDATE ${DatabaseConst.TB_DAILY_RECORD} SET mixed = CAST(totalDistance AS INTEGER);")
-
-        database.execSQL("ALTER TABLE ${DatabaseConst.TB_DETAIL_RECORD} ADD COLUMN poolLength INTEGER NOT NULL DEFAULT 25;")
-        database.execSQL("ALTER TABLE ${DatabaseConst.TB_DETAIL_RECORD} ADD COLUMN crawl INTEGER NOT NULL DEFAULT 0;")
-        database.execSQL("ALTER TABLE ${DatabaseConst.TB_DETAIL_RECORD} ADD COLUMN backStroke INTEGER NOT NULL DEFAULT 0;")
-        database.execSQL("ALTER TABLE ${DatabaseConst.TB_DETAIL_RECORD} ADD COLUMN breastStroke INTEGER NOT NULL DEFAULT 0;")
-        database.execSQL("ALTER TABLE ${DatabaseConst.TB_DETAIL_RECORD} ADD COLUMN butterfly INTEGER NOT NULL DEFAULT 0;")
-        database.execSQL("ALTER TABLE ${DatabaseConst.TB_DETAIL_RECORD} ADD COLUMN kickBoard INTEGER NOT NULL DEFAULT 0;")
-        database.execSQL("ALTER TABLE ${DatabaseConst.TB_DETAIL_RECORD} ADD COLUMN mixed INTEGER NOT NULL DEFAULT 0;")
-        database.execSQL("UPDATE ${DatabaseConst.TB_DETAIL_RECORD} SET mixed = CAST(distance AS INTEGER);")
-
     }
 }
