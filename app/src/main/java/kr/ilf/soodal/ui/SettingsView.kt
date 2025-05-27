a54f2cc269265906da67f5facb8d5b9c4899bb1f
Park MinSeong <qkralstjd02@gmail.com>
2025-05-27 16:51:54 +0900

    설정 타이틀 수정, 주석 삭제

--- app/src/main/java/kr/ilf/soodal/ui/SettingsView.kt
+++ app/src/main/java/kr/ilf/soodal/ui/SettingsView.kt
@@ -2,15 +2,18 @@ package kr.ilf.soodal.ui
 
 import android.app.Activity
 import android.content.Intent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -22,6 +25,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -36,10 +40,10 @@ fun SettingsScreen(navController: NavController) {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Settings") },
+                title = { Text(stringResource(R.string.settings_label_settings)) },
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) {
-                        Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
                     }
                 }
             )
@@ -50,22 +54,27 @@ fun SettingsScreen(navController: NavController) {
 
         OssLicensesMenuActivity.setActivityTitle(openSourceLicensesStr)
 
-        LazyColumn(modifier = Modifier.padding(paddingValues)) {
+        LazyColumn(modifier = Modifier.padding(paddingValues), horizontalAlignment = Alignment.CenterHorizontally) {
             item {
                 SwitchSettingItem(
                     title = "Enable Notifications",
-                    checked = true, // 실제로는 ViewModel에서 상태 관리
-                    onCheckedChanged = { /* ViewModel 업데이트 */ }
+                    checked = true,
+                    onCheckedChanged = {  }
                 )
             }
+
             item {
+                Spacer(Modifier.padding(horizontal = 16.dp).fillMaxWidth().height(0.5.dp).background(Color.Gray))
+
                 TextSettingItem(
                     title = stringResource(R.string.settings_label_app_version),
                     subtitle = BuildConfig.VERSION_NAME,
-                    onClick = { /* 버전 정보 다이얼로그 등 */ }
+                    onClick = { }
                 )
             }
             item {
+                Spacer(Modifier.padding(horizontal = 16.dp).fillMaxWidth().height(0.5.dp).background(Color.Gray))
+
                 TextSettingItem(
                     title = openSourceLicensesStr,
                     subtitle = "",
