4da50cf57953116fd1674a0507d5698a26536d4b
Park MinSeong <qkralstjd02@gmail.com>
2025-06-11 15:54:48 +0900

    SettingsRepository 에newSessionNotificationsEnabled 추가

--- app/src/main/java/kr/ilf/soodal/repository/SettingsRepository.kt
+++ app/src/main/java/kr/ilf/soodal/repository/SettingsRepository.kt
@@ -4,6 +4,8 @@ import kotlinx.coroutines.flow.Flow
 
 interface SettingsRepository {
     val notificationsEnabled: Flow<Boolean>
-
     suspend fun setNotificationsEnabled(enabled: Boolean)
+
+    val newSessionNotificationsEnabled: Flow<Boolean>
+    suspend fun setNewSessionNotificationsEnabled(enabled: Boolean)
 }
--- app/src/main/java/kr/ilf/soodal/repository/SettingsRepositoryImpl.kt
+++ app/src/main/java/kr/ilf/soodal/repository/SettingsRepositoryImpl.kt
@@ -19,34 +19,52 @@ val Context.settingsDataStore: DataStore<Preferences> by preferencesDataStore(na
 
 class SettingsRepositoryImpl(private val context: Context) : SettingsRepository {
 
-    private object PreferencesKeys {
-        val NOTIFICATIONS_ENABLED = booleanPreferencesKey("notifications_enabled")
+    override val notificationsEnabled: Flow<Boolean> =
+        getPreferenceFlow(PreferencesKeys.NOTIFICATIONS_ENABLED, false)
+
+    override suspend fun setNotificationsEnabled(enabled: Boolean) =
+        setPreferenceValue(PreferencesKeys.NOTIFICATIONS_ENABLED, enabled)
+
+
+    override val newSessionNotificationsEnabled: Flow<Boolean> =
+        getPreferenceFlow(PreferencesKeys.NEW_SESSION_NOTIFICATIONS_ENABLED, false)
+
+    override suspend fun setNewSessionNotificationsEnabled(enabled: Boolean) =
+        setPreferenceValue(PreferencesKeys.NEW_SESSION_NOTIFICATIONS_ENABLED, enabled)
+
+
+    // Worker에서 사용할 수 있도록 단일 값을 가져오는 함수
+    suspend fun getNotificationsEnabledOnce(): Boolean =  notificationsEnabled.first()
+    suspend fun getNewSessionNotificationsEnabledOnce(): Boolean =  newSessionNotificationsEnabled.first()
+
+
+    private fun <T> getPreferenceFlow(key: Preferences.Key<T>, defaultValue: T): Flow<T> {
+        return context.settingsDataStore.data
+            .catch { exception ->
+                if (exception is IOException) {
+                    emit(emptyPreferences())
+                } else {
+                    throw exception
+                }
+            }
+            .map { preferences ->
+                preferences[key] ?: defaultValue
+            }
     }
 
-    override val notificationsEnabled: Flow<Boolean> = context.settingsDataStore.data
-        .catch { exception ->
-            if (exception is IOException) {
-                emit(emptyPreferences())
-            } else {
-                throw exception
-            }
-        }
-        .map { preferences ->
-            preferences[PreferencesKeys.NOTIFICATIONS_ENABLED] ?: false
-        }
-
-    override suspend fun setNotificationsEnabled(enabled: Boolean) {
+    private suspend fun <T> setPreferenceValue(key: Preferences.Key<T>, value: T) {
         try {
             context.settingsDataStore.edit { preferences ->
-                preferences[PreferencesKeys.NOTIFICATIONS_ENABLED] = enabled
+                preferences[key] = value
             }
         } catch (e: Exception) {
             e.printStackTrace()
         }
     }
 
-    // Worker에서 사용할 수 있도록 단일 값을 가져오는 함수 (suspend)
-    suspend fun getNotificationsEnabledOnce(): Boolean {
-        return notificationsEnabled.first() // Flow에서 첫 번째 값을 가져옴
+    private object PreferencesKeys {
+        val NOTIFICATIONS_ENABLED = booleanPreferencesKey("notifications_enabled")
+        val NEW_SESSION_NOTIFICATIONS_ENABLED =
+            booleanPreferencesKey("new_session_notifications_enabled")
     }
 }
