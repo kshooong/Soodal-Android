83ba7101ab9fd14a54d8857b4c3ccfb7a5a9c81e
CS533944 <cs533944@nhbank.nonghyup.com>
2025-11-21 23:24:15 +0900

    앱버전 변경 4 -> 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "kr.ilf.soodal"
         minSdk = 29
         targetSdk = 35
-        versionCode = 4
+        versionCode = 5
         versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
